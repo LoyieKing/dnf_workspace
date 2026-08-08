2aedcd37f0b2561ae4b2f59b20e8aecd0baf4ed3
Loyie King <724180662@163.com>
2026-08-08 19:49:42 +0800

    feat(guild): CUserManager 全实现 + CGuildManager 核心方法（51.4% 符号）
    - CUser 布局（0x8c：DBID/CharNo/GameServer/charInfo/guild/blackList）+ 26 个方法实现
    - CUserManager 三 map 布局 + CreateUser/Insert/Delete/Find 全实现
    - CGuildManager 布局（app+guilds map+scheduler+CGuildWar）+ Insert/Delete/Find 等
    - CGuild 基础声明（GetGuildKey/GetGuildName/GetGuildDBFlag）

--- dnf_decompile/source/guild/GuildDomain.cpp
+++ dnf_decompile/source/guild/GuildDomain.cpp
@@ -3,6 +3,10 @@
 
 #include "GuildDomain.h"
 #include "GuildApp.h"
+#include "GuildServer.h"
+#include "GuildPacket.h"
+#include "DNFFileLog.h"
+#include "DNFFunctionLib.h"
 
 CScheduler::CScheduler()
 {
@@ -48,16 +52,236 @@ int CScheduler::GetSpecificDayScheduleHour(int day)
 
 CUser::CUser()
 {
-    memset(m_data, 0, sizeof(m_data));
+    m_dbid = 0;
+    m_charNo = 0;
+    m_gameServer = 0;
+    m_tcpGameServer = 0;
+    m_posState = 0;
+    m_channel = -1;
+    memset(m_charInfo, 0, sizeof(m_charInfo));
+    m_job = 0xff;
+    m_growthType = 0xff;
+    m_guildMemFlag = 0xffff;
+    m_field3a = 1;
+    memset(m_field3b, 0, sizeof(m_field3b));
+    m_guild = 0;
+    m_field48 = 0;
+    memset((void*)&m_guildDBInfo, 0, sizeof(m_guildDBInfo));
+    m_field7c = 0;
+    m_field7e = 0x7f;
+    m_field80 = 0;
+    m_field84 = 0;
 }
 
 CUser::~CUser()
 {
 }
 
+void* CUser::operator new(unsigned int size)
+{
+    return malloc(size);
+}
+
+void CUser::operator delete(void* p)
+{
+    free(p);
+}
+
+void CUser::operator delete(void* p, unsigned int size)
+{
+    free(p);
+}
+
+unsigned int CUser::GetDBID()
+{
+    return m_dbid;
+}
+
+void CUser::SetDBID(unsigned int dbid)
+{
+    m_dbid = dbid;
+}
+
+unsigned int CUser::GetUniqCharNo()
+{
+    return m_charNo;
+}
+
+void CUser::SetUniqCharNo(unsigned int charNo)
+{
+    m_charNo = charNo;
+}
+
+int CUser::GetIdByChannel()
+{
+    return m_channel;
+}
+
+void CUser::SetIdByChannel(int channel)
+{
+    m_channel = channel;
+}
+
+CGameServer* CUser::GetGameServer()
+{
+    return m_gameServer;
+}
+
+void CUser::SetGameServer(CGameServer* server)
+{
+    m_gameServer = server;
+}
+
+CTcpGameServer* CUser::GetTcpGameServer()
+{
+    return m_tcpGameServer;
+}
+
+void CUser::SetTcpGameServer(CTcpGameServer* server)
+{
+    m_tcpGameServer = server;
+}
+
+void CUser::SetUserPosState(unsigned char state)
+{
+    m_posState = state;
+}
+
+char* CUser::GetCharName()
+{
+    return m_charInfo;
+}
+
+void CUser::ResetCharInfo()
+{
+    m_charNo = 0;
+    ResetGuild();
+    m_job = 0xff;
+    m_growthType = 0xff;
+    m_guildMemFlag = 0xffff;
+    memset(m_charInfo, 0, 0x1e);
+}
+
+void CUser::SetUserInfo_CharNo(char sex, char job, short flag, unsigned int charNo, char* name)
+{
+    m_job = (unsigned char)sex;
+    m_growthType = (unsigned char)job;
+    *(short*)((char*)this + 0x38) = flag;
+    m_charNo = charNo;
+    memcpy(m_charInfo, name, 0x1d);
+}
+
+unsigned char CUser::GetJob()
+{
+    return m_job;
+}
+
+unsigned char CUser::GetGrowthType()
+{
+    return m_growthType;
+}
+
+unsigned char CUser::GetSex()
+{
+    return 0;
+}
+
+void CUser::SetSex(unsigned char sex)
+{
+}
+
+void CUser::SetSsn(char* ssn)
+{
+}
+
+char* CUser::GetSsn()
+{
+    return 0;
+}
+
+unsigned char CUser::GetLevel()
+{
+    return 0;
+}
+
+unsigned int CUser::GetGuildKey()
+{
+    return m_guild ? m_guild->GetGuildKey() : 0;
+}
+
+void CUser::ResetGuild()
+{
+    DetachGuild();
+    memset((void*)&m_guildDBInfo, 0, 0x1a);
+    m_field48 = 0;
+    m_field7e = 0x7f;
+    m_field80 = 0;
+    m_field84 = 0;
+}
+
+void CUser::AttachGuild(CGuild* guild)
+{
+    m_guild = guild;
+}
+
+void CUser::DetachGuild()
+{
+    m_guild = 0;
+}
+
+unsigned short CUser::GetGuildMemFlag()
+{
+    return m_field48;
+}
+
+void CUser::SetGuildMemFlag(unsigned short flag)
+{
+    m_field48 = flag;
+}
+
+void CUser::ResetGuildMemFlag(unsigned short flag)
+{
+    m_field48 &= (unsigned short)~flag;
+}
+
+bool CUser::IsSetGuildMemFlag(unsigned short flag)
+{
+    return (m_field48 & flag) != 0;
+}
+
+bool CUser::IsSubGuildMaster()
+{
+    return m_guild != 0 && m_guild->IsSubGuildMaster(m_dbid);
+}
+
+void CUser::SendSetGuildKeyToUser(unsigned int guildKey, unsigned int grade)
+{
+}
+
+void CUser::ChangeCharName(char* name)
+{
+    if (m_charNo != 0 && m_job != 0)
+    {
+        memset(m_charInfo, 0, 0x1e);
+        memcpy(m_charInfo, name, 0x1d);
+    }
+}
+
+void CUser::SendToGameserver(char* buf, int len)
+{
+    if (m_gameServer != 0)
+    {
+        m_gameServer->SendToServer(buf, len);
+    }
+}
+
+void CUser::SendTcpGameserver(PacketHeader* pkt)
+{
+}
+
 CUserManager::CUserManager()
 {
-    memset(m_data, 0, sizeof(m_data));
+    m_app = 0;
 }
 
 CUserManager::~CUserManager()
@@ -66,10 +290,247 @@ CUserManager::~CUserManager()
 
 void CUserManager::Init(CApplication* app)
 {
+    m_app = app;
 }
 
 void CUserManager::Process()
 {
+}
+
+void CUserManager::ProcessByMinute()
+{
+    if (m_app != 0)
+    {
+        CGuildManager* gm = m_app->Get_GuildManager();
+        for (std::map<unsigned int, CUser*>::iterator it = m_users.begin();
+             it != m_users.end(); ++it)
+        {
+            if (it->second->GetGuildKey() != 0)
+            {
+                gm->AttendGuild(it->second->GetGuildKey(), it->second->GetUniqCharNo());
+            }
+        }
+    }
+}
+
+int CUserManager::Size()
+{
+    return (int)m_users.size();
+}
+
+CUser* CUserManager::CreateUser(unsigned int dbid, unsigned int charNo, char* charName,
+                                int channel, CGameServer* server)
+{
+    CUser* user = new CUser;
+    user->SetDBID(dbid);
+    user->SetUniqCharNo(charNo);
+    user->SetIdByChannel(channel);
+    user->SetGameServer(server);
+    if (InsertUser(dbid, user) != 1)
+    {
+        char* mid = NumberToString(dbid, 0);
+        CMyFileLog log("CreateUser", 0x13c);
+        log("./log/LoginErr",
+            "uDBID(%s) uCharNo(%d) is already exist at m_mapUsers!", mid, charNo);
+    }
+    user->SetUserPosState(2);
+    if (charNo != 0)
+    {
+        if (InsertUser_CharNo(charNo, user) != 1)
+        {
+            char* mid = NumberToString(dbid, 0);
+            CMyFileLog log("CreateUser", 0x146);
+            log("./log/LoginErr",
+                "uDBID(%s) uCharNo(%d) is already exist at m_mapCharNoUsers!", mid, charNo);
+        }
+        if (InsertUser_CharName(charName, user) != 1)
+        {
+            char* mid = NumberToString(dbid, 0);
+            CMyFileLog log("CreateUser", 0x14a);
+            log("./log/LoginErr",
+                "uDBID(%s) uCharName(%s) is already exist at m_mapCharNameUsers!", mid, charName);
+        }
+        user->SetUserPosState(3);
+    }
+    return user;
+}
+
+int CUserManager::InsertUser(unsigned int dbid, CUser* user)
+{
+    if (user == 0)
+    {
+        return 0;
+    }
+    m_users.insert(std::make_pair(dbid, user));
+    return 1;
+}
+
+int CUserManager::InsertUser_CharNo(unsigned int charNo, CUser* user)
+{
+    if (user == 0)
+    {
+        return 0;
+    }
+    m_charNoUsers.insert(std::make_pair(charNo, user));
+    return 1;
+}
+
+int CUserManager::InsertUser_CharName(char* name, CUser* user)
+{
+    if (user == 0)
+    {
+        return 0;
+    }
+    m_charNameUsers.insert(std::make_pair(std::string(name), user));
+    return 1;
+}
+
+void CUserManager::DeleteUser(unsigned int dbid)
+{
+    if (m_users.empty())
+    {
+        return;
+    }
+    std::map<unsigned int, CUser*>::iterator it = m_users.find(dbid);
+    if (it != m_users.end())
+    {
+        CUser* user = it->second;
+        if (user->GetGameServer() == 0)
+        {
+            return;
+        }
+        if (m_users.erase(dbid) == 1)
+        {
+            char* mid = NumberToString(dbid, 0);
+            CMyFileLog log("DeleteUser", 0x5f);
+            log("./log/User",
+                "[USER LOGOUT] Disconnected User DB ID : %s, Char No : %d , char name:%s\n",
+                mid, user->GetUniqCharNo(), user->GetCharName());
+            delete user;
+        }
+    }
+}
+
+void CUserManager::DeleteUser(CUser* user)
+{
+    if (m_users.empty() || user == 0)
+    {
+        return;
+    }
+    if (user->GetGameServer() == 0)
+    {
+        return;
+    }
+    unsigned int dbid = user->GetDBID();
+    if (m_users.erase(dbid) == 1)
+    {
+        char* mid = NumberToString(dbid, 0);
+        CMyFileLog log("DeleteUser", 0x7d);
+        log("./log/User", "[USER LOGOUT] Disconnected User DB ID : %s\n", mid);
+        delete user;
+    }
+}
+
+void CUserManager::DeleteUser_CharNo(unsigned int charNo)
+{
+    std::map<unsigned int, CUser*>::iterator it = m_charNoUsers.find(charNo);
+    if (it != m_charNoUsers.end())
+    {
+        DeleteUser(it->second);
+        m_charNoUsers.erase(it);
+    }
+}
+
+void CUserManager::DeleteUser_CharName(const std::string& name)
+{
+    std::map<std::string, CUser*>::iterator it = m_charNameUsers.find(name);
+    if (it != m_charNameUsers.end())
+    {
+        DeleteUser(it->second);
+        m_charNameUsers.erase(it);
+    }
+}
+
+CUser* CUserManager::FindUser(unsigned int dbid)
+{
+    std::map<unsigned int, CUser*>::iterator it = m_users.find(dbid);
+    return it == m_users.end() ? 0 : it->second;
+}
+
+CUser* CUserManager::FindUser_CharNo(unsigned int charNo)
+{
+    std::map<unsigned int, CUser*>::iterator it = m_charNoUsers.find(charNo);
+    return it == m_charNoUsers.end() ? 0 : it->second;
+}
+
+CUser* CUserManager::FindUser_CharName(const char* name)
+{
+    std::map<std::string, CUser*>::iterator it = m_charNameUsers.find(std::string(name));
+    return it == m_charNameUsers.end() ? 0 : it->second;
+}
+
+void CUser::ResetBlackList()
+{
+    m_blackList.clear();
+}
+
+void CUserManager::DeleteUsersOnGameServerDown(CGameServer* server)
+{
+    std::vector<unsigned int> toDelete;
+    for (std::map<unsigned int, CUser*>::iterator it = m_users.begin();
+         it != m_users.end(); ++it)
+    {
+        if (it->second->GetGameServer() == server)
+        {
+            toDelete.push_back(it->first);
+        }
+    }
+    for (std::vector<unsigned int>::iterator it = toDelete.begin(); it != toDelete.end(); ++it)
+    {
+        DeleteUser(*it);
+    }
+}
+
+void CUserManager::DeleteUsersOnTcpGameServerDown(CTcpGameServer* server)
+{
+    std::vector<unsigned int> toDelete;
+    for (std::map<unsigned int, CUser*>::iterator it = m_users.begin();
+         it != m_users.end(); ++it)
+    {
+        if (it->second->GetTcpGameServer() == server)
+        {
+            toDelete.push_back(it->first);
+        }
+    }
+    for (std::vector<unsigned int>::iterator it = toDelete.begin(); it != toDelete.end(); ++it)
+    {
+        DeleteUser(*it);
+    }
+}
+
+void CUserManager::DeleteBlackUserOnCharacDelete(unsigned int charNo)
+{
+    std::map<unsigned int, CUser*>::iterator it = m_charNoUsers.find(charNo);
+    if (it != m_charNoUsers.end())
+    {
+        it->second->ResetBlackList();
+    }
+}
+
+void CUserManager::RefreshGuildAttendanceInfo()
+{
+    if (m_app != 0)
+    {
+        CGuildManager* gm = m_app->Get_GuildManager();
+        for (std::map<unsigned int, CUser*>::iterator it = m_users.begin();
+             it != m_users.end(); ++it)
+        {
+            if (it->second->GetGuildKey() != 0)
+            {
+                gm->AttendGuild(it->second->GetGuildKey(), it->second->GetUniqCharNo());
+            }
+        }
+    }
 }
 
 CGuildWar::CGuildWar()
@@ -90,9 +551,62 @@ int CGuildWar::IsGuildWarEnterableGuild(unsigned int guildId)
     return 0;
 }
 
-CGuildManager::CGuildManager()
+CGuild::CGuild(unsigned int guildKey)
 {
     memset(m_data, 0, sizeof(m_data));
+}
+
+CGuild::~CGuild()
+{
+}
+
+void* CGuild::operator new(unsigned int size)
+{
+    return malloc(size);
+}
+
+void CGuild::operator delete(void* p)
+{
+    free(p);
+}
+
+void CGuild::operator delete(void* p, unsigned int size)
+{
+    free(p);
+}
+
+unsigned int CGuild::GetGuildKey()
+{
+    return *(unsigned int*)((char*)this + 4);
+}
+
+bool CGuild::IsSubGuildMaster(unsigned int dbid)
+{
+    return false;
+}
+
+bool CGuild::IsGuildMaster(unsigned int dbid)
+{
+    return false;
+}
+
+char* CGuild::GetGuildName()
+{
+    return (char*)((char*)this + 0x20);
+}
+
+unsigned short CGuild::GetGuildDBFlag()
+{
+    return *(unsigned short*)((char*)this + 0x14);
+}
+
+CGuildManager::CGuildManager()
+{
+    m_app = 0;
+    m_field40 = 1;
+    memset(m_field70, 0, sizeof(m_field70));
+    memset(m_rest, 0, sizeof(m_rest));
+    m_scheduler.SetSpecialDayHour(1, 5);
 }
 
 CGuildManager::~CGuildManager()
@@ -109,6 +623,206 @@ void CGuildManager::Process()
 
 void CGuildManager::DBSaveProcess(CApplication* app, bool force)
 {
+}
+
+void CGuildManager::AttendGuild(unsigned int guildKey, unsigned int charNo)
+{
+}
+
+void CGuildManager::GuildMemLogin(unsigned int guildKey, CUser* user)
+{
+}
+
+void CGuildManager::GuildMemLogout(unsigned int guildKey, CUser* user)
+{
+}
+
+void CGuildManager::GuildEnter(unsigned int guildKey, ST_Notice_Guild_Enter& info)
+{
+}
+
+void CGuildManager::GuildSecede(unsigned int guildKey, ST_Notice_Guild_Secede& info)
+{
+}
+
+void CGuildManager::CreateGuild(unsigned int guildKey, CServerHandler* handler,
+                                unsigned int masterId)
+{
+}
+
+void CGuildManager::DeleteGuild(unsigned int guildKey)
+{
+    if (m_guilds.empty())
+    {
+        return;
+    }
+    std::map<unsigned int, CGuild*>::iterator it = m_guilds.find(guildKey);
+    if (it == m_guilds.end())
+    {
+        CMyFileLog log("DeleteGuild", 0xcd);
+        log("./log/Guild",
+            "[DELETE_ERR]  Guild Key : %d\tCurr Guild Load Cnt : %d\n",
+            guildKey, (int)m_guilds.size());
+        return;
+    }
+    CMyFileLog log("DeleteGuild", 0xc4);
+    log("./log/Guild",
+        "[DELETE]  Guild Key : %d\tGuild Name : %s\tGuild State:%d\tCurr Guild Load Cnt : %d\n",
+        guildKey, it->second->GetGuildName(),
+        it->second->GetGuildDBFlag() & 0xffff, (int)m_guilds.size() - 1);
+    delete it->second;
+    m_guilds.erase(it);
+}
+
+void CGuildManager::DeleteGuild(CGuild* guild)
+{
+    if (m_guilds.empty() || guild == 0)
+    {
+        return;
+    }
+    if (m_guilds.erase(guild->GetGuildKey()) == 1)
+    {
+        CMyFileLog log("DeleteGuild", 0xe0);
+        log("./log/Guild",
+            "[DELETE]  Guild Key : %d\tGuild Name : %s\tCurr Guild Load Cnt : %d\n",
+            guild->GetGuildKey(), guild->GetGuildName(), (int)m_guilds.size());
+        delete guild;
+    }
+    else
+    {
+        CMyFileLog log("DeleteGuild", 0xe7);
+        log("./log/Guild",
+            "[DELETE_ERR]  Guild Key : %d\tCurr Guild Load Cnt : %d\n",
+            guild->GetGuildKey(), (int)m_guilds.size());
+    }
+}
+
+void CGuildManager::GuildDismiss(CGuild* guild)
+{
+}
+
+int CGuildManager::InsertGuild(unsigned int guildKey, CGuild* guild)
+{
+    if (guild == 0)
+    {
+        return 0;
+    }
+    m_guilds.insert(std::make_pair(guildKey, guild));
+    return 1;
+}
+
+CGuild* CGuildManager::FindGuild(unsigned int guildKey)
+{
+    if (m_guilds.empty())
+    {
+        return 0;
+    }
+    std::map<unsigned int, CGuild*>::iterator it = m_guilds.find(guildKey);
+    return it == m_guilds.end() ? 0 : it->second;
+}
+
+bool CGuildManager::IsEmptyGuild(unsigned int guildKey)
+{
+    return FindGuild(guildKey) == 0;
+}
+
+void CGuildManager::LoadGuild(unsigned int guildKey, STGuildDBInfoOnly& info, char* name)
+{
+}
+
+void CGuildManager::DBGuildProcess(CServerHandler* handler, bool flag)
+{
+}
+
+void CGuildManager::DBGuildAndGuildMemberSave(CServerHandler* handler)
+{
+}
+
+void CGuildManager::DBLoadAllLoginGuild(CServerHandler* handler)
+{
+}
+
+void CGuildManager::SendGuildInfoToMembers(unsigned int guildKey, bool flag)
+{
+}
+
+int CGuildManager::GetTodayMember(unsigned int guildKey)
+{
+    return 0;
+}
+
+void CGuildManager::InsertTodayMember(unsigned int guildKey, STTodayGuildMember& member)
+{
+}
+
+void CGuildManager::RefreshTodayMember(bool flag)
+{
+}
+
+void CGuildManager::RefreshAttendanceInfo(bool flag)
+{
+}
+
+void CGuildManager::CheckAchieveAttendance(unsigned int guildKey)
+{
+}
+
+void CGuildManager::RewardAttendance(unsigned int guildKey, unsigned int charNo,
+                                     unsigned int flag)
+{
+}
+
+int CGuildManager::GetAttendancePhase(unsigned int guildKey)
+{
+    return 0;
+}
+
+void CGuildManager::GetAttendanceInfo(unsigned int guildKey, STAttendanceInfo& info)
+{
+}
+
+int CGuildManager::GetAttendanceExp(unsigned int guildKey, unsigned int phase)
+{
+    return 0;
+}
+
+unsigned int CGuildManager::GetGuildExpWithLevel(unsigned char level)
+{
+    return 0;
+}
+
+int CGuildManager::GetGuildLevelWithExp(unsigned int exp)
+{
+    return 0;
+}
+
+unsigned int CGuildManager::GetMaxGuildExp1()
+{
+    return 0;
+}
+
+unsigned int CGuildManager::GetMaxGuildExp2()
+{
+    return 0;
+}
+
+void CGuildManager::SetGuildExpTable(unsigned int* table)
+{
+}
+
+bool CGuildManager::IsGuildWarEventOn(unsigned char group)
+{
+    return false;
+}
+
+bool CGuildManager::IsGuildWarEnterableChar(unsigned char group, unsigned int charNo)
+{
+    return false;
+}
+
+CGuildWar* CGuildManager::GetGuildWar()
+{
+    return &m_guildWar;
 }
 
 void CGuildManager::CargoLock()
--- dnf_decompile/source/guild/GuildDomain.h
+++ dnf_decompile/source/guild/GuildDomain.h
@@ -12,6 +12,16 @@ class CUser;
 class CGameServer;
 class CTcpGameServer;
 class PacketHeader;
+class CServerHandler;
+class CBlackUser;
+class CGuild;
+struct STBlackUserDBType;
+
+// ---- STGuildMemerDBInfo ----
+struct STGuildMemerDBInfo
+{
+    char m_data[0x20];
+};
 
 // ---- CScheduler：0x8 ----
 class CScheduler
@@ -34,13 +44,96 @@ public:
     char m_flag2;     // +7
 };
 
-// ---- CUser ----
+// ---- CUser：0x8c ----
 class CUser
 {
 public:
     CUser();
     ~CUser();
-    char m_data[0x200];
+    static void* operator new(unsigned int size);
+    static void operator delete(void* p);
+    static void operator delete(void* p, unsigned int size);
+    unsigned int GetDBID();
+    void SetDBID(unsigned int dbid);
+    unsigned int GetUniqCharNo();
+    void SetUniqCharNo(unsigned int charNo);
+    int GetIdByChannel();
+    void SetIdByChannel(int channel);
+    CGameServer* GetGameServer();
+    void SetGameServer(CGameServer* server);
+    CTcpGameServer* GetTcpGameServer();
+    void SetTcpGameServer(CTcpGameServer* server);
+    void SetUserPosState(unsigned char state);
+    char* GetCharName();
+    void ChangeCharName(char* name);
+    void ResetCharInfo();
+    void SetUserInfo_CharNo(char sex, char job, short flag, unsigned int charNo, char* name);
+    unsigned char GetJob();
+    unsigned char GetGrowthType();
+    unsigned char GetSex();
+    void SetSex(unsigned char sex);
+    void SetSsn(char* ssn);
+    char* GetSsn();
+    unsigned char GetLevel();
+    unsigned int GetGuildKey();
+    void ResetGuild();
+    void AttachGuild(CGuild* guild);
+    void DetachGuild();
+    unsigned short GetGuildMemFlag();
+    void SetGuildMemFlag(unsigned short flag);
+    void ResetGuildMemFlag(unsigned short flag);
+    bool IsSetGuildMemFlag(unsigned short flag);
+    bool IsSubGuildMaster();
+    void SetGuildInviteFact(unsigned int guildId, unsigned int callerId, unsigned char fact);
+    unsigned int GetGuildInviteGuildId();
+    unsigned int GetGuildInviteCallerId();
+    void GuildInviteProcess();
+    void SetGuildMemberMemo(const char* memo);
+    void AddGuildMemberPoint(unsigned int point);
+    void ResetGuildPoint();
+    void SetUserChangableInfo(unsigned char type, char value);
+    void SendTcpGameserver(PacketHeader* pkt);
+    void SendToGameserver(char* buf, int len);
+    void QueryGuildMember(CServerHandler* handler);
+    void SendGuildMemberDBInfo(const STGuildMemerDBInfo& info);
+    void LoadGuildMember(unsigned int guildKey, STGuildMemerDBInfo& info);
+    void SaveGuildMember(unsigned char type, unsigned int value, CServerHandler* handler,
+                         unsigned char flag);
+    void ChangeGuildMemberGrade(unsigned char grade);
+    void SendSetGuildKeyToUser(unsigned int guildKey, unsigned int grade);
+    void MakeGameServerSendUserInfoPacket(unsigned int charNo);
+    void RegisterToBlackList(unsigned int charNo, char* name);
+    void RegisterToBlackList(unsigned int charNo, char* name, unsigned int param);
+    void DeleteToBlackList(unsigned int charNo);
+    void ResetBlackList();
+    int IsBlackUser(unsigned int charNo);
+    void GetBlackList(unsigned char* count, STBlackUserDBType* list);
+    void GetBlackList(unsigned char* count, unsigned int* list);
+    unsigned short GetBlackListSize();
+    unsigned short GetBlackListDBFlag();
+    void SetBlackListDBFlag(unsigned short flag);
+    void* GetMapBlackList();
+    void RegisterToCashBlackList(std::map<unsigned int, CBlackUser*>& map);
+    unsigned int m_dbid;              // +0
+    unsigned int m_charNo;            // +4
+    CGameServer* m_gameServer;        // +8
+    CTcpGameServer* m_tcpGameServer;  // +0xc
+    unsigned char m_posState;         // +0x10
+    int m_channel;                    // +0x14
+    char m_charInfo[0x1e];            // +0x18（name@0x18, level/job/sex 等）
+    unsigned char m_job;              // +0x36
+    unsigned char m_growthType;       // +0x37
+    unsigned short m_guildMemFlag;    // +0x38
+    unsigned char m_field3a;          // +0x3a
+    char m_field3b[7];                // +0x3b
+    CGuild* m_guild;                  // +0x44
+    unsigned short m_field48;         // +0x48
+    STGuildMemerDBInfo m_guildDBInfo; // +0x4a
+    std::map<unsigned int, CBlackUser*> m_blackList;  // +0x64
+    unsigned short m_field7c;         // +0x7c
+    char m_field7e;                   // +0x7e
+    unsigned int m_field80;           // +0x80
+    unsigned int m_field84;           // +0x84
 };
 
 // ---- CUserManager：0x4c ----
@@ -51,7 +144,28 @@ public:
     ~CUserManager();
     void Init(CApplication* app);
     void Process();
-    char m_data[0x4c];
+    void ProcessByMinute();
+    int Size();
+    CUser* CreateUser(unsigned int dbid, unsigned int charNo, char* charName, int channel,
+                      CGameServer* server);
+    void DeleteUser(unsigned int dbid);
+    void DeleteUser(CUser* user);
+    void DeleteUser_CharNo(unsigned int charNo);
+    void DeleteUser_CharName(const std::string& name);
+    int InsertUser(unsigned int dbid, CUser* user);
+    int InsertUser_CharNo(unsigned int charNo, CUser* user);
+    int InsertUser_CharName(char* name, CUser* user);
+    CUser* FindUser(unsigned int dbid);
+    CUser* FindUser_CharNo(unsigned int charNo);
+    CUser* FindUser_CharName(const char* name);
+    void DeleteUsersOnGameServerDown(CGameServer* server);
+    void DeleteUsersOnTcpGameServerDown(CTcpGameServer* server);
+    void DeleteBlackUserOnCharacDelete(unsigned int charNo);
+    void RefreshGuildAttendanceInfo();
+    std::map<unsigned int, CUser*> m_users;         // +0
+    std::map<unsigned int, CUser*> m_charNoUsers;   // +0x18
+    std::map<std::string, CUser*> m_charNameUsers;  // +0x30
+    CApplication* m_app;                            // +0x48
 };
 
 // ---- CGuildWar ----
@@ -65,6 +179,29 @@ public:
     char m_data[0x100];
 };
 
+struct ST_Notice_Guild_Enter;
+struct ST_Notice_Guild_Secede;
+struct STGuildDBInfoOnly;
+struct STTodayGuildMember;
+struct STAttendanceInfo;
+
+// ---- CGuild ----
+class CGuild
+{
+public:
+    CGuild(unsigned int guildKey);
+    ~CGuild();
+    static void* operator new(unsigned int size);
+    static void operator delete(void* p);
+    static void operator delete(void* p, unsigned int size);
+    unsigned int GetGuildKey();
+    bool IsSubGuildMaster(unsigned int dbid);
+    bool IsGuildMaster(unsigned int dbid);
+    char* GetGuildName();
+    unsigned short GetGuildDBFlag();
+    char m_data[0x600];
+};
+
 // ---- CGuildManager：0xe0 ----
 class CGuildManager
 {
@@ -73,10 +210,52 @@ public:
     ~CGuildManager();
     void Init(CApplication* app);
     void Process();
+    void ProcessByMinute();
     void DBSaveProcess(CApplication* app, bool force);
+    void AttendGuild(unsigned int guildKey, unsigned int charNo);
+    void GuildMemLogin(unsigned int guildKey, CUser* user);
+    void GuildMemLogout(unsigned int guildKey, CUser* user);
+    void GuildEnter(unsigned int guildKey, ST_Notice_Guild_Enter& info);
+    void GuildSecede(unsigned int guildKey, ST_Notice_Guild_Secede& info);
+    void CreateGuild(unsigned int guildKey, CServerHandler* handler, unsigned int masterId);
+    void DeleteGuild(unsigned int guildKey);
+    void DeleteGuild(CGuild* guild);
+    void GuildDismiss(CGuild* guild);
+    int InsertGuild(unsigned int guildKey, CGuild* guild);
+    CGuild* FindGuild(unsigned int guildKey);
+    bool IsEmptyGuild(unsigned int guildKey);
+    void LoadGuild(unsigned int guildKey, STGuildDBInfoOnly& info, char* name);
+    void DBGuildProcess(CServerHandler* handler, bool flag);
+    void DBGuildAndGuildMemberSave(CServerHandler* handler);
+    void DBLoadAllLoginGuild(CServerHandler* handler);
+    void SendGuildInfoToMembers(unsigned int guildKey, bool flag);
+    int GetTodayMember(unsigned int guildKey);
+    void InsertTodayMember(unsigned int guildKey, STTodayGuildMember& member);
+    void RefreshTodayMember(bool flag);
+    void RefreshAttendanceInfo(bool flag);
+    void CheckAchieveAttendance(unsigned int guildKey);
+    void RewardAttendance(unsigned int guildKey, unsigned int charNo, unsigned int flag);
+    int GetAttendancePhase(unsigned int guildKey);
+    void GetAttendanceInfo(unsigned int guildKey, STAttendanceInfo& info);
+    int GetAttendanceExp(unsigned int guildKey, unsigned int phase);
+    unsigned int GetGuildExpWithLevel(unsigned char level);
+    int GetGuildLevelWithExp(unsigned int exp);
+    unsigned int GetMaxGuildExp1();
+    unsigned int GetMaxGuildExp2();
+    void SetGuildExpTable(unsigned int* table);
+    bool IsGuildWarEventOn(unsigned char group);
+    bool IsGuildWarEnterableChar(unsigned char group, unsigned int charNo);
+    CGuildWar* GetGuildWar();
     void CargoLock();
     void CargoUnlock();
-    char m_data[0xe0];
+    bool IsCargoLock();
+    CApplication* m_app;          // +0
+    std::map<unsigned int, CGuild*> m_guilds;  // +4
+    CScheduler m_scheduler;       // +0x1c
+    unsigned char m_field40;      // +0x40
+    CGuildWar m_guildWar;         // +0x44
+    int m_field70[0x2c / 4];      // +0x70
+    char m_rest[0x8c];            // +0x9c-0xe0
 };
 
 // ---- CPowerWarGuildInfo / CPowerWarCharacInfo ----
@@ -164,9 +343,4 @@ public:
 };
 
 // ---- 基础结构 ----
-struct STGuildMemerDBInfo
-{
-    char m_data[0x20];
-};
-
 #endif  // GUILD_DOMAIN_H_
