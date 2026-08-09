8378c6dccb89edf59c617e37d2906c8dd8de4064
Loyie King <724180662@163.com>
2026-08-09 19:15:49 +0800

    dbmw: 按类拆分第四阶段（TCPSocket/CPeer/CThreadInterface）
    ManagerTypes.cpp 14131->13490 行；构建通过

---
+++ dnf_decompile/source/dbmw/CThreadInterface.cpp
@@ -0,0 +1,62 @@
+#include "ManagerTypes.h"
+#include "ServerXmlDbmw.h"
+#include "ManagerApp.h"
+#include "PacketNameTables.h"
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+#include <unistd.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <sys/resource.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <sys/times.h>
+
+#include "DNFFileLog.h"
+#include "DNFFunctionLib.h"
+
+int getErrno();
+
+CThreadInterface::CThreadInterface()
+{
+    m_thread = 0;
+    m_stop = 0;
+}
+
+CThreadInterface::~CThreadInterface() {}
+
+static void* thread_proxy(void* param)
+{
+    return ((CThreadInterface*)param)->dispatch_proxy(param);
+}
+
+char CThreadInterface::begin()
+{
+    int ret = pthread_create(&m_thread, 0, thread_proxy, this);
+    if (ret < 0)
+    {
+        puts("[ThreadInterface::begin] Can't begin thread");
+        return 0;
+    }
+    return 1;
+}
+
+void* CThreadInterface::dispatch_proxy(void* param)
+{
+    return dispatch(param);
+}
+
+void CThreadInterface::join()
+{
+    pthread_join(m_thread, 0);
+}
+
+void CThreadInterface::stop() { m_stop = 0; }
