7296e2148b6e2fdf41e5b124b20f45a234ebffed
Loyie King <724180662@163.com>
2026-08-09 21:54:17 +0800

    tencrypt: CFastDes 完成（Eric Young 老版 libdes fast DES：fsetkey PC1 查表+28位轮转+7组 hKS/lKS PC2、fencrypt IP/16轮 D_ENCRYPT/FP，TenFastDes 包装），fsetkey/fencrypt 与二进制逐字节一致

--- dnf_secsvr/tencrypt_new/inc/fastdes.h
+++ dnf_secsvr/tencrypt_new/inc/fastdes.h
@@ -15,21 +15,21 @@ struct CFastDes { // line 12
     // sizeof = 128
     struct keysched {
     public:
-    CFastDes::keystage KS[];
+    CFastDes::keystage KS[16];
     };
 private:
 void *m_pSys;
 void *m_pLocal;
 static const unsigned char FUCKING_PRESHIFT[];
-static const unsigned int hKS_C4[][];
-static const unsigned int lKS_D4[][];
+static const unsigned int hKS_C4[7][16];
+static const unsigned int lKS_D4[7][16];
 static const unsigned int wL_I8[];
 static const unsigned int wO_L4[];
-static const unsigned int wPS[][];
-static const unsigned int wC_K4[][];
-static const unsigned int wC_K3[][];
-static const unsigned int wD_K4[][];
-static const unsigned int wD_K3[][];
+static const unsigned int wPS[8][64];
+static const unsigned int wC_K4[128];
+static const unsigned int wC_K3[64];
+static const unsigned int wD_K4[128];
+static const unsigned int wD_K3[64];
 public:
 CFastDes(); // line 14
 ~CFastDes(); // line 15
@@ -40,28 +40,5 @@ private:
 void fencrypt(unsigned char *arg0, int arg1, CFastDes::keysched *arg2); // line 31
 void fsetkey(unsigned char *arg0, CFastDes::keysched *arg1); // line 32
 };
-extern const unsigned char FUCKING_PRESHIFT[]; // line 45
-extern const unsigned int hKS_C4[][]; // line 71
-extern const unsigned int lKS_D4[][]; // line 72
-extern const unsigned int wL_I8[]; // line 74
-extern const unsigned int wO_L4[]; // line 75
-extern const unsigned int wPS[][]; // line 76
-extern const unsigned int wC_K4[][]; // line 78
-extern const unsigned int wC_K3[][]; // line 79
-extern const unsigned int wD_K4[][]; // line 80
-extern const unsigned int wD_K3[][]; // line 81
-namespace CFastDes {
-    // sizeof = 8
-    struct keystage { // line 23
-    public:
-    unsigned int h;
-    unsigned int l;
-    };
-    // sizeof = 128
-    struct keysched { // line 27
-    public:
-    CFastDes::keystage KS[];
-    };
-} // namespace CFastDes
 
 #endif // SECSVR_INC_FASTDES_H_H_
