12f3d7019b30d3f6b3f1ba8d8577c2dad2f236a1
Loyie King <724180662@163.com>
2026-08-09 21:02:42 +0800

    tencrypt: CDesToo 完成（4.3BSD-Reno des.c 封装：fsf/round/doencrypt/dodecrypt/too_setkey/spinit + 静态 SP 表 + TenDesToo），与二进制逐字节一致

--- dnf_secsvr/tencrypt_new/inc/destoo.h
+++ dnf_secsvr/tencrypt_new/inc/destoo.h
@@ -7,20 +7,10 @@
 // sizeof = 6280
 struct CDesToo { // line 7
 private:
-unsigned char too_iperm[][][];
-unsigned char too_fperm[][][];
-unsigned int too_sp[][];
-unsigned char too_kn[][];
-static const unsigned char FUCKING_TOO_PC1[];
-static const unsigned int FUCKING_TOO_BYTEBIT[];
-static const unsigned char FUCKING_TOO_TOTROT[];
-static const unsigned char FUCKING_TOO_PC2[];
-static const unsigned char FUCKING_TOO_IP[];
-static const unsigned char FUCKING_TOO_FP[];
-static const unsigned int FUCKING_TOO_NIBBLEBIT[];
-static const unsigned char FUCKING_TOO_P32I[];
-static const unsigned char FUCKING_TOO_SI[][];
-static const unsigned int FUCKING_TOO_SP[][];
+unsigned char too_iperm[16][16][8];
+unsigned char too_fperm[16][16][8];
+unsigned int too_sp[8][64];
+unsigned char too_kn[16][8];
 void *m_pSys;
 void *m_pLocal;
 public:
@@ -32,22 +22,12 @@ void DesToo(unsigned char *arg0, bool arg1, unsigned char *arg2, unsigned int ar
 private:
 void too_setkey(unsigned char *arg0); // line 17
 void spinit(); // line 18
-void too_perminit(unsigned char *arg0[][], const unsigned char *arg1); // line 19
+void too_perminit(unsigned char (*arg0)[16][8], const unsigned char *arg1); // line 19
 void dodecrypt(unsigned char *arg0); // line 20
 void doencrypt(unsigned char *arg0); // line 21
 void round(int arg0, unsigned int *arg1); // line 22
-void too_permute(unsigned char *arg0, unsigned char *arg1[][], unsigned char *arg2); // line 23
+void too_permute(unsigned char *arg0, unsigned char (*arg1)[16][8], unsigned char *arg2); // line 23
 unsigned int fsf(unsigned int arg0, unsigned char *arg1); // line 24
 };
-extern const unsigned char FUCKING_TOO_PC1[]; // line 40
-extern const unsigned int FUCKING_TOO_BYTEBIT[]; // line 41
-extern const unsigned char FUCKING_TOO_TOTROT[]; // line 42
-extern const unsigned char FUCKING_TOO_PC2[]; // line 43
-extern const unsigned char FUCKING_TOO_IP[]; // line 44
-extern const unsigned char FUCKING_TOO_FP[]; // line 45
-extern const unsigned int FUCKING_TOO_NIBBLEBIT[]; // line 46
-extern const unsigned char FUCKING_TOO_P32I[]; // line 47
-extern const unsigned char FUCKING_TOO_SI[][]; // line 48
-extern const unsigned int FUCKING_TOO_SP[][]; // line 49
 
 #endif // SECSVR_INC_DESTOO_H_H_
